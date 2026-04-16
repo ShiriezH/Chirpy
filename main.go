53cd1a63e577400d665d2800935b113059805db7
ShiriezH <shiriezhendriks1@gmail.com>
2026-04-16 15:58:49 +0200

    Initial Chirpy server with file server and logo

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", fileServer)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
